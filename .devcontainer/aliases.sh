e2f2f951631c9e52d0910802680a4c0348dda907
Josh Garverick <josh.garverick@gmail.com>
2024-02-24 14:41:18 +0000

    Adding aliases

---
+++ .devcontainer/aliases.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Creating shared aliases for the devcontainer
+alias gco='git checkout'
+alias gcb='git checkout -b'
+alias gcm='git commit -m'
+alias ga2='git add .'
+alias gref='git reflog'
+alias gdf='git diff'
