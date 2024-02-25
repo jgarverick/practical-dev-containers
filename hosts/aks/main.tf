86e8364101a83110a62f158351b084fcbc0bbe91
Josh Garverick <josh.garverick@gmail.com>
2024-02-25 04:56:18 +0000

    Minor update to AKS tf

--- hosts/aks/main.tf
+++ hosts/aks/main.tf
@@ -51,7 +51,6 @@ resource "helm_release" "nginx_ingress" {
   name       = "nginx-ingress"
   repository = "https://charts.bitnami.com/bitnami"
   chart      = "nginx-ingress-controller"
-  version    = "8.10.2"
 
   set {
     name  = "controller.service.loadBalancerIP"
